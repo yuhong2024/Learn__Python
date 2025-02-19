8d7cc1670b4f128f183fa71fb6e5f3370c35a950
Yuhong Wang <wyhstar@email.swu.edu.cn>
2025-02-20 01:39:25 +0800

    文件读取

--- code/第八章 文件操作/01_文件的读取.py
+++ code/第八章 文件操作/01_文件的读取.py
@@ -1,10 +1,14 @@
-
+import time
 
 # 打开文件
 f = open("D:/测试.txt","r",encoding="utf-8")
 print(type(f))
 # 读取文件 -read（）
-# re
+
+lines = f.readlines()
+print(lines)
+print(f"read方法读取全部内容的结果：{f.read()}")
+
 
 # 读取文件 -readlines()
 
@@ -15,3 +19,12 @@ print(type(f))
 # 文件的关闭
 
 # with open 语法操作文件
+
+
+
+
+
+f = open("D:/test.txt","w",encoding="utf-8")
+f.write("hello world!")
+f.flush()
+time.sleep(6000000)
